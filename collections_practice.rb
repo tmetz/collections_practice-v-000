08db2e0225a7995195ce87a28acb21e8118dea2b
Tammy Metz <tammy@tammymetz.com>
2018-07-28 06:34:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y<=> x}
+end
+
+def sort_array_char_count(array)
+
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker(input_string)
+
+end
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
